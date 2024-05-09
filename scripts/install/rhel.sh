39d7631280573a52a4c48aa47d77ed2407f12407
sawyerlin <sawyer.lin@gmail.com>
2024-05-09 23:26:40 +0800

    fix rhel

--- scripts/install/rhel.sh
+++ scripts/install/rhel.sh
@@ -209,6 +209,7 @@ else
     done
 
     # install py39
+    rm -rf /tmp/Python-3.9.18
     wget -P /tmp https://www.python.org/ftp/python/3.9.18/Python-3.9.18.tgz
     
     cd /tmp && tar -xvf Python-3.9.18.tgz && cd Python-3.9.18 && \
