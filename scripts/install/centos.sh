876f90ace0b2143d6ab15dab9be8f392fdb78378
sawyerlin <sawyer.lin@gmail.com>
2024-05-10 22:41:39 +0800

    move to another python version

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -159,10 +159,10 @@ if [ "$VERSION_ID" -eq "9" ];then
 fi
 
 # install py39
-rm -rf /tmp/Python-3.9.6
-wget -P /tmp https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+rm -rf /tmp/Python-3.9.18
+wget -P /tmp https://www.python.org/ftp/python/3.9.18/Python-3.9.18.tgz
 
-cd /tmp && tar -xvf Python-3.9.6.tgz && cd Python-3.9.6 && \
+cd /tmp && tar -xvf Python-3.9.18.tgz && cd Python-3.9.18 && \
 ./configure --enable-optimizations && \
 make && make altinstall
 ln -sf /usr/local/bin/python3.9 /usr/bin/python3
