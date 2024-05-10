54d6890e99982017bfb632a8222a543cf2baafb5
sawyerlin <sawyer.lin@gmail.com>
2024-05-10 22:54:07 +0800

    move py39 to rhel

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -158,16 +158,6 @@ if [ "$VERSION_ID" -eq "9" ];then
 	# yum remove -y chardet
 fi
 
-# install py39
-rm -rf /tmp/Python-3.9.18
-wget -P /tmp https://www.python.org/ftp/python/3.9.18/Python-3.9.18.tgz
-
-cd /tmp && tar -xvf Python-3.9.18.tgz && cd Python-3.9.18 && \
-./configure --enable-optimizations && \
-make && make altinstall
-ln -sf /usr/local/bin/python3.9 /usr/bin/python3
-python3 -m ensurepip
-
 cd /www/server/mdserver-web/scripts && bash lib.sh
 chmod 755 /www/server/mdserver-web/data
 
