f6eb84bee9ff8b0ee8d96d9900fede53267a7acf
sawyerlin <sawyer.lin@gmail.com>
2024-05-19 16:24:55 +0800

    rewrite speed check script

---
+++ speed_check.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.getcwd() + "/class/core")
+
+from speed_lib import speed_check_task
+
+speed_check_task()
--- task_test.py
+++
@@ -1,3 +0,0 @@
-import task
-
-task.speed_check_task()
